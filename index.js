a4f25f9b59fce90a45a846641f4558ea3ab0e1db
Simon Gabriell <simongabriellmoreiradesouza@gmail.com>
2025-02-16 00:26:42 -0300

    correcao de bug

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ let sinal=prompt('qual sera o sinal ?')
 let num2=Number(prompt('qual o outro numero'))
 let resultado=0
 
-if(sinal="+"){
+if(sinal==="+"){
     resultado=(num1+num2);
-}   else if(sinal='-'){
+}   else if(sinal==='-'){
     resultado=(num1-num2)
-    }else if (sinal="*"){
+    }else if (sinal==="*"){
         resultado=(num1*num2)
-    }else if(sinal='/'){
+    }else if(sinal==='/'){
         resultado=(num1/num2)
     }else {
         console.log=('voce digitou um sinal invalido, tente novamente')
